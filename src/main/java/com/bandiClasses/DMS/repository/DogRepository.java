16810b8125507261638550dd89a013e77e71c971
SainathBosss <sainathreddyteneti@gmail.com>
2023-04-28 17:48:10 -0500

    First commit

---
+++ src/main/java/com/bandiClasses/DMS/repository/DogRepository.java
@@ -0,0 +1,16 @@
+package com.bandiClasses.DMS.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bandiClasses.DMS.Models.Dog;
+
+/**
+ * @author S556186
+ *
+ */
+public interface DogRepository extends CrudRepository<Dog, Integer>{
+
+	List<Dog> findByName(String name);
+}
---
+++ src/main/java/com/bandiClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,14 @@
+package com.bandiClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bandiClasses.DMS.Models.Trainer;
+
+/**
+ * @author S556186
+ *
+ */
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+	
+}
